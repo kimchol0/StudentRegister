4e9b1c7e079dc3a3f6c5a7106204690ac9cd6a55
KimChol <kimchol2ece@outlook.com>
2020-10-18 11:00:23 +0800

    根据到stu应用下创建的访问路径跳转到views.py中创建函数，如果为GET请求则跳转到register.html页面，否则返回空值

--- stu/views.py
+++ stu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        return None
