a1945baf66537e69e0ed619ff37ca600c28d58e3
KimChol <kimchol2ece@outlook.com>
2020-10-18 16:22:32 +0800

    编辑数据库模型文件(加入on_delete参数,否则会报异常)

--- stu/models.py
+++ stu/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
 class Clazz(models.Model):
     cno = models.AutoField(primary_key=True)
     cname = models.CharField(max_length=30)
 
     def __str__(self):
-        return u'Clazz:%s'%self.cname
+        return u'Clazz:%s' % self.cname
+
 
 class Course(models.Model):
     course_no = models.AutoField(primary_key=True)
     course_name = models.CharField(max_length=30)
 
     def __str__(self):
-        return u'Course:%s'%self.course_name
+        return u'Course:%s' % self.course_name
+
 
 class Student(models.Model):
     sno = models.AutoField(primary_key=True)
     sname = models.CharField(max_length=30)
-    cls = models.ForeignKey(Clazz)
+    cls = models.ForeignKey(Clazz,on_delete=models.CASCADE)
     cour = models.ManyToManyField(Course)
 
     def __str__(self):
-        return u'Student:%s'%self.sname
+        return u'Student:%s' % self.sname
